c1a6bd2e5590e6a714cc0caab85bac48e2d5e55e
Baptiste LE ROUX <bapt.leroux29@gmail.com>
2024-10-10 15:56:13 +0200

    u update transcription.rs

--- src/midi/transcription.rs
+++ src/midi/transcription.rs
@@ -1,5 +1,3 @@
-// src/midi/transcription.rs
-
 use midir::{MidiOutput, MidiOutputConnection};
 use std::error::Error;
 use std::thread;
@@ -7,32 +5,54 @@ use std::time::Duration;
 
 pub struct MidiTranscriber {
     conn_out: MidiOutputConnection,
+    channel: u8, // Canal MIDI (0-15)
 }
 
 impl MidiTranscriber {
-    pub fn new() -> Result<Self, Box<dyn Error>> {
+    pub fn new(channel: u8) -> Result<Self, Box<dyn Error>> {
+        if channel > 15 {
+            return Err("Le canal MIDI doit être compris entre 0 et 15.".into());
+        }
+
         // Initialiser la sortie MIDI
         let midi_out = MidiOutput::new("My MIDI Output")?; 
-        // Choisir le port de sortie MIDI (par exemple, le premier disponible)
+        // Choisir le port de sortie MIDI (par exemple, FluidSynth)
         let out_ports = midi_out.ports();
         if out_ports.is_empty() {
             return Err("Aucun port de sortie MIDI disponible".into());
         }
 
-        // Pour une application réelle, vous devriez permettre à l'utilisateur de choisir le port
-        let port = &out_ports[0];
+        // Afficher les ports disponibles pour choisir le bon
+        for (i, port) in out_ports.iter().enumerate() {
+            println!("Port {}: {}", i, midi_out.port_name(port)?);
+        }
+
+        // Demander à l'utilisateur de choisir le port
+        println!("Entrez le numéro du port MIDI à utiliser:");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+        let port_index: usize = input.trim().parse()?;
+        if port_index >= out_ports.len() {
+            return Err("Index de port invalide".into());
+        }
+
+        let port = &out_ports[port_index];
         let conn_out = midi_out.connect(port, "midir-test")?;
 
-        Ok(MidiTranscriber { conn_out })
+        Ok(MidiTranscriber { conn_out, channel })
     }
 
     pub fn play_note(&mut self, note: u8, velocity: u8, duration_ms: u64) -> Result<(), Box<dyn Error>> {
+        // Construire les messages Note ON et Note OFF en spécifiant le canal
+        let note_on = 0x90 + self.channel; // 0x90 est le code pour Note ON sur le canal 1
+        let note_off = 0x80 + self.channel; // 0x80 est le code pour Note OFF sur le canal 1
+
         // Note ON
-        self.conn_out.send(&[0x90, note, velocity])?;
+        self.conn_out.send(&[note_on, note, velocity])?;
         // Attendre la durée spécifiée
         thread::sleep(Duration::from_millis(duration_ms));
         // Note OFF
-        self.conn_out.send(&[0x80, note, 0])?;
+        self.conn_out.send(&[note_off, note, 0])?;
         Ok(())
     }
 }
